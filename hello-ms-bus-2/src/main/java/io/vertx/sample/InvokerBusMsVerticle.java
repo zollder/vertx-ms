01c311b6ec32fa5aeffd6a6712329eefa3ed0a19
Eugene Simon <goodzollder@gmail.com>
2018-03-12 02:03:01 -0400

    Added event bus -based invoker microservice

---
+++ hello-ms-bus-2/src/main/java/io/vertx/sample/InvokerBusMsVerticle.java
@@ -0,0 +1,31 @@
+package io.vertx.sample;
+
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.core.eventbus.Message;
+import rx.Single;
+
+public class InvokerBusMsVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        vertx.createHttpServer()
+                .requestHandler(
+                        request -> {
+                            EventBus bus = vertx.eventBus();
+                            Single<JsonObject> obs1 = bus.<JsonObject>rxSend("hello", "test1").map( Message::body);
+                            Single<JsonObject> obs2 = bus.<JsonObject>rxSend("hello", "test2").map( Message::body);
+                            Single.zip(obs1, obs2, (test1, test2) -> new JsonObject()
+                                    .put("test1", test1.getString("message"))
+                                    .put("test2", test2.getString("message")))
+                                    .subscribe(
+                                            success -> request.response().end(success.encodePrettily()),
+                                            error -> {
+                                                error.printStackTrace();
+                                                request.response().setStatusCode(500).end(error.getMessage());
+                                            });
+                        })
+                .listen(8082);
+    }
+}
